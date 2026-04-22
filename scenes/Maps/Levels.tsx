fae8412fa4610f6f06454bebd56bd95ea2d2b671
SrG640 <sergifloresvaro@elpuig.xeill.net>
2026-04-22 18:28:40 +0200

    Primera Prueba

---
+++ scenes/Maps/Levels.tsx
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Levels" tilewidth="10" tileheight="10" tilecount="25" columns="5">
+ <image source="../../assets/TileSpriteSheet.png" width="50" height="50"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="10" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0444444" y="6.97778">
+    <polygon points="0,0 10.0444,-0.0222222 10.0444,2.13333 0.0666667,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="7.95556" width="10" height="2.04444"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="1.02222" y="7.11111" width="7.86667" height="1.93333"/>
+  </objectgroup>
+ </tile>
+</tileset>
